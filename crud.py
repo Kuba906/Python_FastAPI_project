f83474cb8495fb7d48cdacff87cdaca3cc26e79c
Kuba906 <63562993+Kuba906@users.noreply.github.com>
2021-05-19 09:47:08 +0200

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func, update
+from fastapi import HTTPException
 import models
 import schemas
-from sqlalchemy import update, func
-from fastapi import HTTPException
+
 
 def get_shippers(db: Session):
     return db.query(models.Shipper).all()
@@ -16,33 +17,44 @@ def get_shipper(db: Session, shipper_id: int):
 
 def get_suppliers(db: Session):
     return db.query(models.Supplier)\
-        .order_by(models.Supplier.SupplierID)\
+             .order_by(models.Supplier.SupplierID.asc())\
+             .all()
+
+
+def get_supplier(db: Session, id: int):
+    return (
+        db.query(models.Supplier)
+          .filter(models.Supplier.SupplierID == id)
+          .first()
+    )
+
+
+def get_products_from_supplier(db: Session, id: int):
+    return (
+        db.query(models.Product)
+            .filter(models.Product.SupplierID == id)
+            .order_by(models.Product.ProductID.desc())
             .all()
-
-def get_supplier(db: Session, supplier_id: int):
-    return(db.query(models.Supplier)
-        .filter(models.Supplier.SupplierID == supplier_id)
-        .first())
-
-def get_supplier_products(db: Session, supplier_id:int):
-    return db.query(models.Product)\
-        .join(models.Category).filter(models.Product.SupplierID == supplier_id)\
-            .order_by(models.Product.ProductID.desc()).all()
+    )
 
 
 def create_supplier(db: Session, new_supplier: schemas.NewSupplier):
-    id = db.query(func.max(models.Supplier.SupplierID)).scalar()
-    new_supplier.SupplierID = id + 1
+    highest_id = db.query(func.max(models.Supplier.SupplierID)).scalar()
+    new_supplier.SupplierID = highest_id + 1
     db.add(models.Supplier(**new_supplier.dict()))
     db.commit()
-    return get_supplier(db, id + 1)
+    return get_supplier(db, highest_id + 1)
 
-def update_supplier(db: Session, id: int, supplier_update: schemas.UpdateSupplier):
-    supplier_dict = {key: el for key, el in supplier_update.dict().items() if el is not None}
-    update = update(models.Supplier).where(models.Supplier.SupplierID == id).values(**supplier_dict)
-    db.execute(update)
+
+def update_supplier(db: Session, id: int, supplier_update: schemas.SupplierUpdate):
+    properties_to_update = {key: value for key, value in supplier_update.dict().items() if value is not None}
+    update_statement = update(models.Supplier) \
+                       .where(models.Supplier.SupplierID == id) \
+                       .values(**properties_to_update)
+    db.execute(update_statement)
     db.commit()
     return get_supplier(db, id)
+
 
 def delete_supplier(db: Session, id: int):
     check_supplier = get_supplier(db, id)
