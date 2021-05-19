c690ef1916d0f64460b15115cfdf2cba99202129
Kuba906 <63562993+Kuba906@users.noreply.github.com>
2021-05-19 09:38:15 +0200

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from . import models
-from schemas import NewSupplier, UpdateSupplier
+import schemas
 from sqlalchemy import update, func
 
 
@@ -20,14 +20,14 @@ def get_supplier_products(db: Session, supplier_id:int):
             .order_by(models.Product.ProductID.desc()).all()
 
 
-def create_supplier(db: Session, new_supplier: NewSupplier):
+def create_supplier(db: Session, new_supplier: schemas.NewSupplier):
     id = db.query(func.max(models.Supplier.SupplierID)).scalar()
     new_supplier.SupplierID = id + 1
     db.add(models.Supplier(**new_supplier.dict()))
     db.commit()
     return get_supplier(db, id + 1)
 
-def update_supplier(db: Session, id: int, supplier_update: UpdateSupplier):
+def update_supplier(db: Session, id: int, supplier_update: schemas.UpdateSupplier):
     supplier_dict = {key: el for key, el in supplier_update.dict().items() if el is not None}
     update = update(models.Supplier).where(models.Supplier.SupplierID == id).values(**supplier_dict)
     db.execute(update)
